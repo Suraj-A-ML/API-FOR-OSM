9e93eababb074428b55d5afa3ed5010f7d9f8e51
Suraj-A-ML <124239645+Suraj-A-ML@users.noreply.github.com>
2023-04-13 12:15:14 +0530

    updated the path to upload and unzip

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import os
-
-import json
-
 import zipfile
 import subprocess
-from flask import jsonify
-import ast
 
 
-#coding = utf8
 app = Flask(__name__)
 def inv_path():
-  #path = app.config['UNZIP_FOLDER'] + '/'
-  #return os.path.join(path,'inventory.yaml')
-  return '/home/ubuntu/osm/inventory.yaml'
+  path = app.config['OSM_FOLDER'] + '/'
+  return os.path.join(path,'inventory.yaml')
 
 def ins_path():
-  path = app.config['UPLOAD_FOLDER'] + '/'
+  path = app.config['OSM_FOLDER'] + '/'
   return os.path.join(path,'install_scaling_manager.yaml')
-  return '/home/ubuntu/osm/install_scaling_manager.yaml'
-  
+
 def pem_path():
   return os.path.abspath("user-dev-aws-ssh.pem")
-  
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_zip():
@@ -55,25 +46,26 @@ def populate():
     master_ip = request.get_json('master_ip')
     os_user = request.get_json('os_user')
     os_pass = request.get_json('os_pass')
-    command  = ['sudo', 'ansible-playbook', '-i', inv_path() ,ins_path(), '--tags', "populate_inventory_yaml" ,'-e master_node_ip={}'.format(master_ip),'-e os_user={}'.format(os_user),'-e os_pass={}'.format(os_pass)]
+    command  = ['sudo', 'ansible-playbook', '-i', inv_path() ,ins_path(), '--tags', "populate_inventory_yaml" ,'-e master_node_ip={}'.format(master_ip),'-e os_user={}'.format(os_user),'-e os_pass={}'.format(os_pass),'-kK']
     subprocess.run(command)
     return "Scaling manager population is successful"
-    
+#@app.route('/ans',methods=['GET','POST'])
+#def ans():
+#       res = subprocess.run(['ansible','--version'])
+#       print(res.stdout)
+#       print(res.stderr)
+#       return "done"
+
 @app.route('/install')
 def install_scaling_manager():
+    command = ['sudo', 'ansible-playbook', '-i', inv_path() ,ins_path(), '--tags', "install" ,'--key-file',pem_path(),'-e','src_bin_path="."']
+    subprocess.run(command)
+    return 'Scaling manager installation started.'
 
-    command = ['sudo', 'ansible-playbook', '-i', inv_path(), ins_path(), '--tags', 'install', '--key-file', pem_path(), '-e', 'src_bin_path="."']
-    output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    json_output = json.dumps({'stdout': output.stdout.decode('utf-8')})
-    return json_output
-    
- 
 @app.route('/start')
 def start_scaling_manager():
     command = ['sudo', 'ansible-playbook', '-i', inv_path() ,ins_path(), '--tags', "start" ,'--key-file',pem_path(),'-e','src_bin_path="."']
     subprocess.run(command)
-    return "Started"
 
 @app.route('/stop')
 def stop_scaling_manager():
@@ -96,13 +88,9 @@ def update_scaling_manager():
 #to develop a status end poin
 @app.route('/status')
 def status_scaling_manager():
-    #inv_path ='/home/ubuntu/osm/inventory.yaml'
-    #ins_path ='/home/ubuntu/osm/install_scaling_manager.yaml'
-    #pem_path = os.path.abspath("user-dev-aws-ssh.pem")
     command = ['sudo','ansible-playbook', '-i', inv_path() ,ins_path(), '--tags', "status" ,'--key-file',pem_path(),'-e','src_bin_path="."']
     subprocess.run(command)
     return 'Status is returned.'
-
 
 @app.route('/update_pem')
 def updatepem_scaling_manager():
@@ -110,10 +98,13 @@ def updatepem_scaling_manager():
     subprocess.run(command)
     return 'Pem files is updated.'
 
-if __name__ == '__main__':
-    app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(),"upload")
-    app.config['UNZIP_FOLDER'] = os.path.join(os.getcwd(),"unzipped")
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['UNZIP_FOLDER'], exist_ok=True)
-    app.run(debug=True,host="0.0.0.0")
 
+
+
+if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = "/home/ubuntu/uploads"
+    app.config['OSM_FOLDER'] = "/home/ubuntu/osm"
+    mode = 0o766
+    os.makedirs(app.config['UPLOAD_FOLDER'], mode=mode, exist_ok=True)
+    os.makedirs(app.config['OSM_FOLDER'],mode=mode, exist_ok=True)
+    app.run(debug=True)
